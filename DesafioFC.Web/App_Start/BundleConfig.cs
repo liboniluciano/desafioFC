a722b63e9dcd560a074e54097386a6ed3de28ac1
Luciano Henrique Silva Filho <luciano_liboni@hotmail.com>
2018-06-12 01:15:19 -0300

    criação do crud de agendamento e melhoramento da view principal

--- DesafioFC.Web/App_Start/BundleConfig.cs
+++ DesafioFC.Web/App_Start/BundleConfig.cs
@@ -9,22 +9,25 @@ namespace DesafioFC.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender. Em seguida, quando estiver
             // pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquerymask").Include(
+                "~/Scripts/jquery.mask.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
         }
     }
 }
---
+++ DesafioFC.Web/Controllers/AgendamentoController.cs
@@ -0,0 +1,89 @@
+﻿using DesafioFC.Data;
+using DesafioFC.Domain;
+using System.Web.Mvc;
+
+namespace DesafioFC.Web.Controllers
+{
+    public class AgendamentoController : Controller
+    {
+        private readonly AgendamentoData _agendamentoData;
+
+        public AgendamentoController()
+        {
+            _agendamentoData = new AgendamentoData();
+        }
+
+        public ActionResult Index()
+        {
+            var agendamentos = _agendamentoData.ListarAgendamentos();
+            return View(agendamentos);
+        }
+
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Novo(Agendamento agendamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _agendamentoData.Salvar(agendamento);
+                return RedirectToAction("Index");
+            }
+
+            return View(agendamento);
+        }
+
+        public ActionResult Detalhes(string id)
+        {
+            var agendamento = _agendamentoData.ListarAgendamento(id);
+            if (agendamento == null)
+                return HttpNotFound();
+
+            return View(agendamento);
+        }
+
+        public ActionResult Editar(string id)
+        {
+            var agendamento = _agendamentoData.ListarAgendamento(id);
+            if (agendamento == null)
+                return HttpNotFound();
+
+            return View(agendamento);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Agendamento agendamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _agendamentoData.Salvar(agendamento);
+                return RedirectToAction("Index");
+            }
+
+            return View(agendamento);
+        }
+
+        public ActionResult Excluir(string id)
+        {
+            var agendamento = _agendamentoData.ListarAgendamento(id);
+            if (agendamento == null)
+                return HttpNotFound();
+
+            return View(agendamento);
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirAgendamento(string id)
+        {
+            var agendamento = _agendamentoData.ListarAgendamento(id);
+            _agendamentoData.Excluir(agendamento);
+            return RedirectToAction("Index");
+        }
+    }
+}
