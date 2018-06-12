ee89a7d4e704d38c3482dcc60bb3ae4a0ef5e6b6
Luciano Henrique Silva Filho <luciano_liboni@hotmail.com>
2018-06-12 00:22:21 -0300

    criação do crud de usuario

---
+++ DesafioFC.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,88 @@
+﻿using System.Web.Mvc;
+using DesafioFC.Data;
+using DesafioFC.Domain;
+
+namespace DesafioFC.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly UsuarioData _usuarioData;
+
+        public UsuarioController()
+        {
+            _usuarioData = new UsuarioData();
+        }
+        public ActionResult Index()
+        {
+            var usuarios = _usuarioData.ListarUsuarios();
+            return View(usuarios);
+        }
+
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Novo(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioData.Salvar(usuario);
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        public ActionResult Detalhes(string id)
+        {
+            var usuario = _usuarioData.ListarUsuario(id);
+            if (usuario == null)
+                return HttpNotFound();
+
+            return View(usuario);
+        }
+
+        public ActionResult Alterar(string id)
+        {
+            var usuario = _usuarioData.ListarUsuario(id);
+            if (usuario == null)
+                return HttpNotFound();
+
+            return View(usuario);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Alterar(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioData.Salvar(usuario);
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        public ActionResult Excluir(string id)
+        {
+            var usuario = _usuarioData.ListarUsuario(id);
+            if (usuario == null)
+                return HttpNotFound();
+
+            return View(usuario);
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirId(string id)
+        {
+            var usuario = _usuarioData.ListarUsuario(id);
+            _usuarioData.Excluir(usuario);
+            return RedirectToAction("Index");
+        }
+    }
+}
