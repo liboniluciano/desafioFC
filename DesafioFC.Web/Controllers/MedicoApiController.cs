cc2eca2ee0190c490e81cb0a57b3aaf3235d8eca
Luciano Henrique Silva Filho <luciano_liboni@hotmail.com>
2018-06-12 00:46:40 -0300

    criando crud de médicos com api para recuperar lista de médicos

---
+++ DesafioFC.Web/Controllers/MedicoApiController.cs
@@ -0,0 +1,115 @@
+﻿using DesafioFC.Data;
+using DesafioFC.Domain;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace DesafioFC.Web.Controllers
+{
+    public class MedicoApiController : ApiController
+    {
+        private Context db = new Context();
+
+        // GET: api/MedicoApi
+        public IQueryable<Medico> GetMedicos()
+        {
+            return db.Medicos;
+        }
+
+        // GET: api/MedicoApi/5
+        [ResponseType(typeof(Medico))]
+        public IHttpActionResult GetMedico(int id)
+        {
+            Medico medico = db.Medicos.Find(id);
+            if (medico == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(medico);
+        }
+
+        // PUT: api/MedicoApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutMedico(int id, Medico medico)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != medico.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(medico).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MedicoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/MedicoApi
+        [ResponseType(typeof(Medico))]
+        public IHttpActionResult PostMedico(Medico medico)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Medicos.Add(medico);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = medico.Id }, medico);
+        }
+
+        // DELETE: api/MedicoApi/5
+        [ResponseType(typeof(Medico))]
+        public IHttpActionResult DeleteMedico(int id)
+        {
+            Medico medico = db.Medicos.Find(id);
+            if (medico == null)
+            {
+                return NotFound();
+            }
+
+            db.Medicos.Remove(medico);
+            db.SaveChanges();
+
+            return Ok(medico);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MedicoExists(int id)
+        {
+            return db.Medicos.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ DesafioFC.Web/Controllers/MedicoController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DesafioFC.Data;
+using DesafioFC.Domain;
+
+namespace DesafioFC.Web.Controllers
+{
+    public class MedicoController : Controller
+    {
+        private readonly MedicoData _medicoData;
+
+        public MedicoController()
+        {
+            _medicoData = new MedicoData();
+        }
+
+        public ActionResult Index()
+        {
+            var medicoApi = new MedicoApiController();
+            var medicos = medicoApi.GetMedicos();
+            return View(medicos);
+        }
+
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Novo(Medico medico)
+        {
+            if (ModelState.IsValid)
+            {
+                _medicoData.Salvar(medico);
+                return RedirectToAction("Index");
+            }
+
+            return View(medico);
+        }
+
+        public ActionResult Detalhes(string id)
+        {
+            var medico = _medicoData.ListarMedico(id);
+            if (medico == null)
+                return HttpNotFound();
+
+            return View(medico);
+        }
+
+        public ActionResult Alterar(string id)
+        {
+            var medico = _medicoData.ListarMedico(id);
+            if (medico == null)
+                return HttpNotFound();
+
+            return View(medico);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Alterar(Medico medico)
+        {
+            if (ModelState.IsValid)
+            {
+                _medicoData.Salvar(medico);
+                return RedirectToAction("Index");
+            }
+
+            return View(medico);
+        }
+
+        public ActionResult Excluir(string id)
+        {
+            var medico = _medicoData.ListarMedico(id);
+            if (medico == null)
+                return HttpNotFound();
+
+            return View(medico);
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirId(string id)
+        {
+            var medico = _medicoData.ListarMedico(id);
+            _medicoData.Excluir(medico);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
